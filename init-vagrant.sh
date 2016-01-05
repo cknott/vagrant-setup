48cc5ed25288bbf597f77822197149627dbdcee5
Christian Knott <ck@cknott.net>
2016-01-06 00:53:07 +0100

    added plugin

--- init-vagrant.sh
+++ init-vagrant.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 vagrant plugin install vagrant-cachier
 vagrant plugin install vagrant-hostmanager
-vagrant plugin install vagrant plugin install vagrant-auto_network
+vagrant plugin install vagrant-auto_network
+vagrant plugin install vagrant-vbguest
 
